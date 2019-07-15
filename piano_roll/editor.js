f72ebded7564187c59038af9724d67830461f216
Sam Hodak <slhodak@gmail.com>
2019-07-14 18:43:13 -0700

    testing out viewport creation scripts

--- piano_roll/editor.js
+++ piano_roll/editor.js
@@ -1,4 +1,14 @@
 //  Editor for sequencer -- View & Controller
-let editor = '<div>This is the editor</div>';
+let editor = document.createElement('div');
+editor.setAttribute('class', 'viewport');
+let roll = document.createElement('div');
+roll.setAttribute('class', 'roll');
+for (let i = 0; i < 88; i++) {
+  let note = document.createElement('div');
+  note.setAttribute('class', 'noteLine');
+  roll.append(note);
+}
+
+editor.append(roll);
 
 export default editor;
