6d13372992207ed51994d53992729cc8f16fbff8
Sam Hodak <slhodak@gmail.com>
2019-07-15 15:05:39 -0700

    Move score tests to test file

--- piano_roll/score.js
+++ piano_roll/score.js
@@ -68,24 +68,6 @@ const TriggerNode = function (time, event) {
   this.next = null;
 };
 
-
-let score = new Score();
-
-score.insertEvent(15, [128, 2, 3]);
-score.insertEvent(20, [144, 2, 3]);
-score.insertEvent(16, [128, 5, 15]);
-score.insertEvent(24, [144, 5, 14]);
-score.insertEvent(16, [128, 15, 15]);
-score.insertEvent(22, [144, 15, 12]);
-
-score.removeEvent(16, [127, 5, 15]);
-
-let node = score.triggers.head;
-while (node) {
-  console.log(node);
-  node = node.next;
-}
-
 export {
   Score,
   TriggerNode
