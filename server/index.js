0658396c8829533fa86f444d024ee4c377657f71
Sam Hodak <slhodak@gmail.com>
2019-07-14 18:13:55 -0700

    Set up express server to serve dist dir

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
 //  Server needed to write and read data from files
+
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = 3001;
+
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+app.listen(port, () => {
+  console.log(`Piano roll service available on port ${port}`);
+});
