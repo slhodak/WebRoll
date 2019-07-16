98f9a8dd18aad6e1b29e08fa915578ba24ece556
Sam Hodak <slhodak@gmail.com>
2019-07-16 15:56:40 -0700

    Add divisions property to editor + test

--- piano_roll/editor.js
+++ piano_roll/editor.js
@@ -3,7 +3,7 @@
 //  Editor = entire module
 //    - Interface to alter Grid Division and Time Signature
 
-const Editor = function(length, timeDivision, timeSignature) {
+const Editor = function(length = 16, timeDivision = [1, 8], timeSignature = [4, 4]) {
   this.length = length;
   this.timeDivision = timeDivision;
   this.timeSignature = timeSignature;
@@ -12,17 +12,18 @@ const Editor = function(length, timeDivision, timeSignature) {
   //    Grid populates based on model data
   //    Grid edits alter model
   //    TODO: Generate based on grid division, t/s, and length (in bars for now)
-  this.createGrid = function(length = 16, timeDivision = [1, 8], timeSignature = [4, 4]) {
-    let grid = document.createElement('div');
+  this.createGrid = function(length, divisions, timeSignature) {
     //  create grid
+    let grid = document.createElement('div');
     //  fill grid with divisions(length, timedivision) rows of divs.
-    let n = numberOfDivisions(length, timeDivision);
-  
+
   };
   this.numberOfDivisions = function(length, timeDivision) {
     return (timeDivision[1] * length) / timeDivision[0];
   };
+  this.divisions = this.numberOfDivisions(length, timeDivision);
 
+  this.createGrid(length, timeDivision, timeSignature);
   //  Insert into Grid Container
   
 }
--- tests/editor.test.js
+++ tests/editor.test.js
@@ -1,11 +1,18 @@
 import Editor from '../piano_roll/editor';
 
 describe('Editor Tests', () => {
-  const editor = new Editor();
-
+  
   it('should get the correct number of divisions for a given bar length and subdivision value', () => {
+    const editor = new Editor();
     let n = editor.numberOfDivisions(1, [1, 4]);
     expect(n).toBe(4);
   });
+  
+  it('should add the number of divisions to a property on the editor instance', () => {
+    const editor = new Editor(1, [1, 4]);
+    expect(editor.divisions).toBe(4);
+  });
+
+  
 });
 
