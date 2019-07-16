65cfa5e351e3766158cece8688bb6ddb3417e908
Sam Hodak <slhodak@gmail.com>
2019-07-16 14:13:55 -0700

    Refactor Editor to class

--- piano_roll/editor.js
+++ piano_roll/editor.js
@@ -3,22 +3,28 @@
 //  Editor = entire module
 //    - Interface to alter Grid Division and Time Signature
 
-//  Create Grid
-//    Grid populates based on model data
-//    Grid edits alter model
-//    TODO: Generate based on grid division, t/s, and length (in bars for now)
-function createGrid(length = 16, timeDivision = [1, 8], timeSignature = [4, 4]) {
-  let grid = document.createElement('div');
-  //  create grid
-  //  fill grid with divisions(length, timedivision) rows of divs.
-  let n = numberOfDivisions(length, timeDivision);
+const Editor = function(length, timeDivision, timeSignature) {
+  this.length = length;
+  this.timeDivision = timeDivision;
+  this.timeSignature = timeSignature;
 
+  //  Create Grid
+  //    Grid populates based on model data
+  //    Grid edits alter model
+  //    TODO: Generate based on grid division, t/s, and length (in bars for now)
+  this.createGrid = function(length = 16, timeDivision = [1, 8], timeSignature = [4, 4]) {
+    let grid = document.createElement('div');
+    //  create grid
+    //  fill grid with divisions(length, timedivision) rows of divs.
+    let n = numberOfDivisions(length, timeDivision);
+  
+  };
+  this.numberOfDivisions = function(length, timeDivision) {
+    return ((64 * timeDivision[0]) / timeDivision[1]) * length;
+  };
+  
+  //  Insert into Grid Container
+  
 }
 
-function numberOfDivisions(length, timeDivision) {
-  return ((64 * timeDivision[0]) / timeDivision[1]) * length;
-}
-
-//  Insert into Grid Container
-
-export default editor;
+export default Editor;
