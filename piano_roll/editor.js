97fb02ba6a2853ebf69fc0a3ea427c7993e9c00d
Sam Hodak <slhodak@gmail.com>
2019-07-16 14:10:05 -0700

    Add grid creation helper func

--- piano_roll/editor.js
+++ piano_roll/editor.js
@@ -6,8 +6,18 @@
 //  Create Grid
 //    Grid populates based on model data
 //    Grid edits alter model
-//    TODO: Generate based on grid division and t/s
+//    TODO: Generate based on grid division, t/s, and length (in bars for now)
+function createGrid(length = 16, timeDivision = [1, 8], timeSignature = [4, 4]) {
+  let grid = document.createElement('div');
+  //  create grid
+  //  fill grid with divisions(length, timedivision) rows of divs.
+  let n = numberOfDivisions(length, timeDivision);
 
+}
+
+function numberOfDivisions(length, timeDivision) {
+  return ((64 * timeDivision[0]) / timeDivision[1]) * length;
+}
 
 //  Insert into Grid Container
 
