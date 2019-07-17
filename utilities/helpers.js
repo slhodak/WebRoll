e1ab3aa27284d7e599b128eb5ebb7e6a670c13e8
Sam Hodak <slhodak@gmail.com>
2019-07-17 12:00:45 -0700

    Change note names to legal classnames

--- utilities/helpers.js
+++ utilities/helpers.js
@@ -1,17 +1,17 @@
 const noteUtils = {
   noteDict: {
     0: ['A'],
-    1: ['A#', 'Bb'],
+    1: ['As', 'Bb'],
     2: ['B'],
     3: ['C'],
-    4: ['C#', 'Db'],
+    4: ['Cs', 'Db'],
     5: ['D'],
-    6: ['D#', 'Eb'],
+    6: ['Ds', 'Eb'],
     7: ['E'],
     8: ['F'],
-    9: ['F#', 'Gb'],
+    9: ['Fs', 'Gb'],
     10: ['G'],
-    11: ['G#', 'Ab']
+    11: ['Gs', 'Ab']
   },
   getNoteByNumber(number) {
     return noteUtils.noteDict[number % 12];
