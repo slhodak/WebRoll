8b62669a77c0ad31cbad4085e181659e1dbc6771
Sam Hodak <slhodak@gmail.com>
2019-07-17 10:10:25 -0700

    Move Editor methods to prototype

--- piano_roll/editor.js
+++ piano_roll/editor.js
@@ -12,20 +12,29 @@ const Editor = function(length = 16, timeDivision = [1, 8], timeSignature = [4, 
   //    Grid populates based on model data
   //    Grid edits alter model
   //    TODO: Generate based on grid division, t/s, and length (in bars for now)
-  this.createGrid = function(length, divisions, timeSignature) {
-    //  create grid
-    let grid = document.createElement('div');
-    //  fill grid with divisions(length, timedivision) rows of divs.
-
-  };
-  this.numberOfDivisions = function(length, timeDivision) {
-    return (timeDivision[1] * length) / timeDivision[0];
-  };
   this.divisions = this.numberOfDivisions(length, timeDivision);
-
   this.createGrid(length, timeDivision, timeSignature);
   //  Insert into Grid Container
-  
-}
+};
+
+Editor.prototype.numberOfDivisions = function(length, timeDivision) {
+  return (timeDivision[1] * length) / timeDivision[0];
+};
+
+Editor.prototype.createGrid = function(length, divisions, timeSignature) {
+  //  create grid
+  let grid = document.createElement('div');
+  //  fill grid with divisions(length, divisions) rows of divs.
+  let columns = '';
+  for (let i = 0; i < this.divisions; i++) {
+    let column = `<div class="timeColumn" data-id=${i}>`;
+    for (let j = 1; j < 89; j++) {
+      column += `<div class="noteCell" data-note=${i}></div>`;
+    }
+    column += '</div>';
+    columns += column;
+  }
+  grid.innerHTML = columns;
+};
 
 export default Editor;
