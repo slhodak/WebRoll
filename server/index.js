d3de37827db96453303f9b75f54f8f70a95c0b7a
Sam Hodak <slhodak@gmail.com>
2019-07-19 01:16:34 -0700

    Connect ws package server to WebSocket client at port 4001

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const wss = require('./websocket');
+
+//  Express Server - Static Files & Bundles
 
 const app = express();
 const { Network } = require('./config');
@@ -10,6 +13,23 @@ const { Network } = require('./config');
 app.use(cors());
 app.use(express.static(path.resolve(__dirname, '../dist')));
 app.use('/piano_roll', express.static(path.resolve(__dirname, '../piano_roll')));
+
+
+//  WebSocket Server - Note Event Queue Transmissions
+
+wss.on('listening', () => {
+  console.log('WebSocket Server listening on port 4001');
+});
+
+wss.on('connection', function connection(ws) {
+  console.log('ws connected');
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+ 
+  ws.send('something');
+});
+
 
 app.listen(Network.httpPort, () => {
   console.log(`Piano roll service available on port ${Network.httpPort}`);
