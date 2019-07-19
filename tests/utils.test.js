ad5429f918d991b8399ab81ae2691905479902b9
Sam Hodak <slhodak@gmail.com>
2019-07-18 18:37:32 -0700

    Add noteutil getNote test, wrap unit tests in describe block

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,7 +1,13 @@
 import { noteUtils } from '../piano_roll/lib/helpers.js';
 
 describe('Utilities Tests', () => {
-  it('should calculate the correct note name by its number', () => {
-    expect(noteUtils.getNoteByNumber(0)).toEqual(['A']);
+  describe('getNoteByNumber', () => {
+    it('should return an array', () => {
+      expect(noteUtils.getNoteByNumber(52)).toBeInstanceOf(Array);
+    });
+  
+    it('should calculate the correct note name by its number', () => {
+      expect(noteUtils.getNoteByNumber(0)).toEqual(['A']);
+    });
   });
 });
