59c33f818df380cbc64bd9e9eea9a9d22d9de2f7
Sam Hodak <slhodak@gmail.com>
2019-08-15 14:20:15 -0700

    clear comment

--- piano_roll/rollSocket.js
+++ piano_roll/rollSocket.js
@@ -4,7 +4,6 @@ const socket = new WebSocket(`${config.rollWSHost}:${config.rollWSPort}`, 'roll'
 
 socket.onopen = (open) => {
   console.log("Opened connection to Roll WebSocket Server");
-  //  tell the roll socket who this is (not a synth)
 };
 
 socket.onmessage = (message) => {
