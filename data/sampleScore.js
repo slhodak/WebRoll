2eecca6a2da46d4a9f9b017a12ab33c604b3761f
Sam Hodak <slhodak@gmail.com>
2019-08-15 15:43:41 -0700

    Fix reference errors

--- data/sampleScore.js
+++ data/sampleScore.js
@@ -1,24 +1,10 @@
-const t16 = {
-  time: 16,
+const t178 = {
+  time: 178,
   events: [
-    [144, 64, 127]
+    [128, 68, 127],
+    [128, 64, 127]
   ],
-  next: t64
-};
-const t64 = {
-  time: 64,
-  events: [
-    [128, 64, 127],
-    [144, 66, 127]
-  ],
-  next: t92
-};
-const t92 = {
-  time: 92,
-  events: [
-    [128, 66, 127]
-  ],
-  next: t128
+  next: null
 };
 const t128 = {
   time: 128,
@@ -28,20 +14,34 @@ const t128 = {
   ],
   next: t178
 };
-const t178 = {
-  time: 178,
+const t92 = {
+  time: 92,
   events: [
-    [128, 68, 127],
-    [128, 64, 127]
+    [128, 66, 127]
   ],
-  next: null
+  next: t128
+};
+const t64 = {
+  time: 64,
+  events: [
+    [128, 64, 127],
+    [144, 66, 127]
+  ],
+  next: t92
+};
+const t16 = {
+  time: 16,
+  events: [
+    [144, 64, 127]
+  ],
+  next: t64
 };
 
 const sampleScore = {
   triggers: {
     head: t16
   },
-  current: score.triggers.head
-}
+  current: t16
+};
 
 module.exports = sampleScore;
